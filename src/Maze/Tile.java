fce868cf92eb089679d01d9b297ec9398fe2a132
mohee69s <srourmohee22@gmail.com>
2025-11-08 19:37:03 +0300

    Init

---
+++ src/Maze/Tile.java
@@ -0,0 +1,16 @@
+package Maze;
+
+public class Tile {
+    public int x;
+    public int y;
+    public int cost;
+    public int health;
+    public boolean end;
+    public Tile(int x, int y, int cost, int health, boolean end) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+        this.health = health;
+        this.end = end;
+    }
+}
---
+++ src/MazeStruct.java
@@ -0,0 +1,9 @@
+import Maze.Tile;
+
+import java.util.HashSet;
+
+public class MazeStruct {
+    public MazeStruct(HashSet<Tile> maze, int height, int width ) {
+
+    }
+}
